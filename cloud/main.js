e0235abd30849628bd685355e9992841682a3dc6
Kenan Pulak <kepulak@gmail.com>
2017-02-26 16:54:52 -0800

    commented out sample code

--- cloud/main.js
+++ cloud/main.js
@@ -1,11 +1,11 @@
 // Get access to Parse Server's cache
 const { AppCache } = require('parse-server/lib/cache');
 
-Parse.Cloud.define('sendWelcomeMail', (request, response) => {
+/*Parse.Cloud.define('sendWelcomeMail', (request, response) => {
   // Get a reference to the MailgunAdapter
     const MailgunAdapter = AppCache.get('appId')['userController']['adapter'];
     MailgunAdapter.send({...});
-});
+});*/
 
 Parse.Cloud.afterSave(Parse.User, function(request) {
 
@@ -14,10 +14,9 @@ Parse.Cloud.afterSave(Parse.User, function(request) {
   MailgunAdapter.send({
     templateName: 'customEmailAlert',
     // Optional override of your configuration's subject
-    subject: 'Important: action required',
+    subject: 'Welcome to The Hive!',
     // Optional override of the adapter's fromAddress
-    fromAddress: 'Alerts <noreply@yourapp.com>',
-    recipient: user.email,
-    variables: { alert: 'New posts' } // {{alert}} will be compiled to 'New posts'
+    fromAddress: 'The Hive <noreply@change-gamer.com>',
+    recipient: user.email
   });
 });
